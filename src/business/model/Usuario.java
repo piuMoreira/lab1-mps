1752639ef1e37b61f5eaa00c0a2e9c68a842b43b
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-05-27 17:56:05 -0300

    adiciona a classe Usuario

--- src/business/model/Usuario.java
+++ src/business/model/Usuario.java
@@ -1,5 +1,15 @@
 package business.model;
 
 public class Usuario {
+    private final String email;
+    private final String password;
 
+    public Usuario(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
