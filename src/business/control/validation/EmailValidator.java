c3ad07dbfa0ace6644b8d21d014c651fa36c8ce3
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-05-29 15:19:41 -0300

    troca por CustomError por CustomException

--- src/business/control/validation/EmailValidator.java
+++ src/business/control/validation/EmailValidator.java
@@ -2,26 +2,24 @@ package business.control.validation;
 
 import business.control.UserInput;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class EmailValidator implements Validator {
 
     @Override
-    public void validate(Map<UserInput, String> value) {
+    public void validate(Map<UserInput, String> value) throws CustomException {
         String email = value.get(UserInput.EMAIL);
 
         if (email.length() == 0) {
-            throw new MissingParam("Por favor, insira seu email");
+            throw new MissingParamException("Por favor, insira seu email");
         }
 
         if (email.length() > 12) {
-            throw new InvalidParam("O email deve ter no máximo 12 caracteres");
+            throw new InvalidParamException("O email deve ter no máximo 12 caracteres");
         }
 
         if (email.matches(".*\\d")) {
-            throw new InvalidParam("O email não deve conter números");
+            throw new InvalidParamException("O email não deve conter números");
         }
     }
 }
--- src/business/control/validation/PasswordValidator.java
+++ src/business/control/validation/PasswordValidator.java
@@ -2,28 +2,24 @@ package business.control.validation;
 
 import business.control.UserInput;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class PasswordValidator implements Validator {
 
     @Override
-    public void validate(Map<UserInput, String> value) {
+    public void validate(Map<UserInput, String> value) throws CustomException {
         String password = value.get(UserInput.PASSWORD);
 
-        List<String> errorValidation = new ArrayList<>();
-
         if (password.length() < 8) {
-            throw new InvalidParam("A senha deve ter no mínimo 8 caracteres");
+            throw new InvalidParamException("A senha deve ter no mínimo 8 caracteres");
         }
 
         if (password.length() > 20) {
-            throw new InvalidParam("A senha deve ter no máximo 20 caracteres");
+            throw new InvalidParamException("A senha deve ter no máximo 20 caracteres");
         }
 
         if (password.matches("(?=(.*\\d){2})(?=(.*[A-Za-z]))ˆ.*")) {
-            throw new InvalidParam("A deve ter letras e no mínimo 2 números");
+            throw new InvalidParamException("A deve ter letras e no mínimo 2 números");
         }
     }
 }
