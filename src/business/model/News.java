d60474f572bc928ac9af1af0d1fd8ddcce865ab5
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-05-29 15:50:00 -0300

    cria a entidade News

---
+++ src/business/model/News.java
@@ -0,0 +1,15 @@
+package business.model;
+
+public class News {
+
+    private String title;
+
+    public News(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
