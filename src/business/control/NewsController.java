3c35752c471784b6a8eb35c0613838fb8acb48a4
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2021-06-01 17:01:07 -0300

    Update NewsController.java
    Modifica para receber o login do usuário no NewsController

--- src/business/control/NewsController.java
+++ src/business/control/NewsController.java
@@ -9,28 +9,33 @@ import business.model.User;
 
 public class NewsController {
 
-    public List<String> add(User user, String title) {
-
+    public List<String> add(String userEmail, String title) {
+        //TODO: criar a função no findUserByEmail
+        User user = findUserByEmail(userEmail)
+            
         List<String> errors = new ArrayList<>();
 
         try {
             News news = new News(user, title);
             //TODO: passar o title para a infra para salvar no arquivo
         } catch (CustomException ex) {
-            //
+            errors.add(ex.getMessage())
         }
 
         return errors;
     }
 
-    public List<String> delete(User user, String title) {
+    public List<String> delete(String userEmail, String title) {
+        //TODO: criar a função no findUserByEmail
+        User user = findUserByEmail(userEmail)
+            
         List<String> errors = new ArrayList<>();
 
 //        try {
 //            // Na infra pesquisar se a news do título {title} criado por {user} existe
 //            //TODO: Passar o email para o infra para deletar (infra deve retornar erro caso o email não exista na base de dados)
 //        } catch (CustomException ex) {
-//            //
+//            errors.add(ex.getMessage())
 //        }
 
         return errors;
