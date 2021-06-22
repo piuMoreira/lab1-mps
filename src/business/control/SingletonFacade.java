b16dc08c703f5b994ee3127844003ece8bf92557
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-06-21 21:57:31 -0300

    remove validador

--- src/business/control/SingletonFacade.java
+++ src/business/control/SingletonFacade.java
@@ -24,7 +24,6 @@ public class SingletonFacade {
     }};
 
     private static final List<Validator> announcementValidators = new ArrayList<>() {{
-        add(new EmailValidator());
         add(new AnnouncementValidator());
     }};
 
