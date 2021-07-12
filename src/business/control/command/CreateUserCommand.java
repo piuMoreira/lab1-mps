95f8339323fe3df5b461704e6c821453c8f29e08
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-07-12 20:20:02 -0300

    cria command para criar usuário e o ValidationComposite

---
+++ src/business/control/command/CreateUserCommand.java
@@ -0,0 +1,21 @@
+package business.control.command;
+
+import business.control.Command;
+import business.util.helpers.UserInput;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CreateUserCommand implements Command {
+
+	@Override
+	public List<String> execute(Map<UserInput, String> userInput) {
+
+        List<String> errors = new ArrayList<>();
+        errors.addAll( userController.add(userInput) );
+
+        return errors;
+	}
+
+}
---
+++ src/business/control/validation/ValidationComposite.java
@@ -0,0 +1,32 @@
+package business.control.validation;
+
+import business.control.validation.exceptions.CustomException;
+import business.util.helpers.UserInput;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ValidationComposite {
+
+    private ArrayList<Validator> validators = new ArrayList<>();
+
+    public ValidationComposite(ArrayList<Validator> validators) {
+        this.validators = validators;
+    }
+
+    public List<String> validate(Map<UserInput, String> userInput) {
+        List<String> errors = new ArrayList<>();
+
+        try {
+            for (Validator validator : this.validators) {
+                validator.validate(userInput);
+            }
+        } catch (CustomException ex) {
+            errors.add(ex.getMessage());
+        }
+
+        return errors;
+    }
+
+}
